8298830efbf469cc4ee3e6827747ea5b89cacd0c
cse18107 <cse18107@cemk.ac.in>
2024-08-07 22:44:57 +0530

    added create city api

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,5 +1,6 @@
 const AirplaneRepository = require("./airplane-repositroy");
-
+const CityRepository = require("./city-repository");
 module.exports = {
-    AirplaneRepository
+  AirplaneRepository,
+  CityRepository,
 };
