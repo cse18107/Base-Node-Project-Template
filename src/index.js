e37f52abd361e4efd12e7e42c4f9156d4b9d5259
cse18107 <cse18107@cemk.ac.in>
2024-08-08 10:51:29 +0530

    added new migration to add foreign key constraint in one to many relation

--- src/index.js
+++ src/index.js
@@ -10,7 +10,28 @@ app.use(express.urlencoded({extended: true}));
 
 app.use("/api", apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async() => {
   console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`);
+  // const { City, Airport } = require("./models");
+  // const bengaluru = await  City.findByPk(1);
+  // // const airport = await Airport.create({name: 'Kempegowda Airport', code: 'BLR', cityId: 1});
+  // const kmpairport = await bengaluru.createAirport({
+  //   name: "Huballi Airport",
+  //   code: "HBL",
+  // });
+  // console.log(kmpairport)
+  // const airportsInBlr = await bengaluru.getAirports();
+  // console.log(airportsInBlr)
+  // await City.destroy({
+  //   where: {
+  //     id: 2
+  //   }
+  // });
+  // const city = await City.findB/ "Indore airport", code: "IND" });
+  // await City.destroy({
+  //   where: {
+  //     id: 3
+  //   }
+  // });
 });
 
